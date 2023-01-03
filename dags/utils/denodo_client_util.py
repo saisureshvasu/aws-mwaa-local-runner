c86b5bb1a66eb4401050a7b6201ea15575b01e1c
saisureshvasu <saisuresh.vasu@cdw.com>
2023-01-03 12:37:27 +0530

    odbc

--- dags/utils/denodo_client_util.py
+++ dags/utils/denodo_client_util.py
@@ -5,10 +5,11 @@ urllib3.disable_warnings()
 log = logging.getLogger(__name__)
 
 import pyodbc
-
+import pymssql
 
 def test_odbc():
     #pass
     print(pyodbc.version)
     print([x for x in pyodbc.drivers()])
+    print([x for x in pymssql.drivers()])
     
