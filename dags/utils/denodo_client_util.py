e6056879abbe81581a23fc36ac3222494d14e2e2
saisureshvasu <saisuresh.vasu@cdw.com>
2023-01-03 19:21:04 +0530

    Reverting to the state of the project at 294eb7f

--- dags/utils/denodo_client_util.py
+++ dags/utils/denodo_client_util.py
@@ -11,4 +11,10 @@ def test_odbc():
     #pass
     print(pyodbc.version)
     print([x for x in pyodbc.drivers()])
-    
+    server = 'zbytlm6git.database.windows.net' 
+    database = 'PF_CCM_Live' 
+    username = 'PF_DataTeam_read' 
+    password = 'h?rx^V5?SQqkuDG5' 
+    # ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
